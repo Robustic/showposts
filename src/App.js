bf34275a2bf942efbdc50f11edb1dc33d324088b
Juha Malinen <juha.malinen@helsinki.fi>
2021-02-01 16:48:05 +0200

    Posts are printed as JSON format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useAsync } from "react-async"
+import axios from 'axios'
+
+const loadPosts = async () => {
+  const response = await axios.get('https://guarded-mountain-76807.herokuapp.com/api/posts')
+  if (!response.statusText === "OK") throw new Error(response.statusText)
+  
+  return response.data
+}
 
 const App = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
+  const { data, error, isPending } = useAsync({ promiseFn: loadPosts})
+  if (isPending) return "Loading..."
+  if (error) return `Something went wrong: ${error.message}`
+  if (data)
+    
+    return (
+      <div>
+        <strong>Posts:</strong>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    )
+  return null
 }
 
 export default App
